913d6b74d0495b64b134786dbd003f0886f1db19
tangx <uyinn@live.com>
2021-08-12 11:16:40 +0800

    style: clean

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,6 @@
 </template>
 
 <script lang="ts">
-import PostDataHelper from "./store/PostDataHelper";
-let dh: PostDataHelper = new PostDataHelper("testpost", "id");
-dh.addData("post01: new post 1");
-dh.addData("post02: new post 2");
-</script>
-
-<script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import MemuBar from "./components/MenuBar.vue";
 
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
-import ItemData from './model/ItemData'
-import Catigory from './model/CataEnums'
 
 Vue.config.productionTip = false
 
@@ -10,8 +8,4 @@ new Vue({
     store,
     render: h => h(App)
 }).$mount('#app')
-
-
-const item2 = new ItemData(1, Catigory.Work, "你好 vue", "这是我用 vue 写的第一个网站")
-console.log("item2::: =>", item2)
 

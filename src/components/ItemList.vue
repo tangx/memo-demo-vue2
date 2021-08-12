8c377911ec9cad00624ca4148e0228b38464399e
tangx <uyinn@live.com>
2021-08-12 18:44:14 +0800

    docs: 增加 ItemList 的描述

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -13,9 +13,13 @@
 <script lang="ts">
 import ItemData from "@/model/ItemData";
 import Vue from "vue";
-import store from "../store";
 
+// 在 main.ts 中， Vue 中已经包含了 store， 所以不需要再导入
+// import store from "../store";
+
+// extends Vue 继承 vue
 export default class ItemList extends Vue {
+  // 使用 this 代指当前 Vue 域
   memoArr: ItemData[] = this.$store.state.aHelper.memoList;
 }
 </script>
--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 
-import CataEnums from './model/CataEnums'
-import ItemData from './model/ItemData'
-
-import ActionHelper from './store/ActionHelper'
-
-
-
-
-
-
 Vue.config.productionTip = false
 
 new Vue({
@@ -19,7 +9,11 @@ new Vue({
     render: h => h(App)
 }).$mount('#app')
 
+
 // 初始化 localStorage 数据
+// import CataEnums from './model/CataEnums'
+// import ItemData from './model/ItemData'
+// import ActionHelper from './store/ActionHelper'
 // const ah = new ActionHelper();
 // const item = new ItemData(-1, CataEnums.Study, "title 2", "content 2")
 // ah.add(item)

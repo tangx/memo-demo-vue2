15aca88d17bd65c6a18cb0b0d471f3389b147a1b
tangx <uyinn@live.com>
2021-08-12 23:58:52 +0800

    feat: 倒叙显示 与 子组件

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -1,38 +1,40 @@
 <template>
   <div id="memos" class="container">
-    <div
-      class="memo"
-      v-for="item in reverseMemoArry()"
-      :key="item.id"
-      :id="item.id"
-    >
-      <div class="title">{{ item.title }}</div>
-      <div class="time">{{ item.createTime }} <button>编辑</button></div>
-
-      <div class="content">
-        {{ item.content }}
-      </div>
+    <div>
+      <MemoItem v-for="item in reverseMemoArr" :key="item.id" :item="item" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import ItemData from "@/model/ItemData";
-import Vue from "vue";
-
 // 在 main.ts 中， Vue 中已经包含了 store， 所以不需要再导入
 // import store from "../store";
 
-// extends Vue 继承 vue
+import ItemData from "@/model/ItemData";
+import MemoItem from "./MemoItem.vue";
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {
+    MemoItem,
+  },
+
+  computed: {
+    reverseMemoArr() {
+      return this.$store.state.aHelper.memoList.reverse();
+    },
+  },
+})
 export default class ItemList extends Vue {
   // 使用 this 代指当前 Vue 域
-  memoArr: ItemData[] = this.$store.state.aHelper.memoList;
-
+  //   memoArr: ItemData[] = this.$store.state.aHelper.memoList;
   // 倒序显示
   // https://blog.csdn.net/Vicki_/article/details/80094602
-  reverseMemoArry(): ItemData[] {
-    return this.memoArr.reverse();
-  }
+  //   reverseMemoArry(): ItemData[] {
+  //     // const rev = this.memoArr.reverse();
+  //     // return rev;
+  //     return this.memoArr.reverse();
+  //   }
 }
 </script>
 
---
+++ src/components/MemoItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="memo" :key="item.id" :memo-id="item.id">
+    <div class="title">{{ item.title }}</div>
+    <div class="time">{{ item.createTime }} <button>编辑</button></div>
+
+    <div class="content">
+      {{ item.content }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import ItemData from "@/model/ItemData";
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component
+export default class MemoItem extends Vue {
+  @Prop() item!: ItemData;
+}
+</script>

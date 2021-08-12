53d098cccd091d5cef5a6f0ff6d93c0b5c208b39
tangx <uyinn@live.com>
2021-08-12 21:52:19 +0800

    chore:

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,10 @@ new Vue({
 }).$mount('#app')
 
 
-// 初始化 localStorage 数据
+// // 初始化 localStorage 数据
 // import CataEnums from './model/CataEnums'
 // import ItemData from './model/ItemData'
 // import ActionHelper from './store/ActionHelper'
 // const ah = new ActionHelper();
-// const item = new ItemData(-1, CataEnums.Study, "title 2", "content 2")
+// const item = new ItemData(-1, CataEnums.Study, "备忘录", "这是一个操作界面友好的备忘录")
 // ah.add(item)
